9750fedf50defffba93d987c18a5e482b5328481
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-09-22 01:25:29 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW5-P5-GrassSeed-JacksonMcKay/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  //first 2 variables
+  double cost;
+  double lawns;
+
+  //ask for input of cost and amt of lawns
+  cin >> cost;
+  cin >> lawns;
+
+  
+}
