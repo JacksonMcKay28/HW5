cd0c668b16d9ea0fcef4915c772783ef5406e6a7
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-09-22 01:36:45 +0000

    for loop partially set up

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 int main() {
   //first 2 variables
   double cost;
-  double lawns;
+  int lawns;
 
   //ask for input of cost and amt of lawns
   cin >> cost;
   cin >> lawns;
 
+  //variables for length width and area
+  double length;
+  double width;
+  double area;
+
+  //for loop for calculating area as many times as there is lawns
+  for(int i = 0; i < lawns; i++ ) {
+    cin >> width >> length;
+  }
   
 }
